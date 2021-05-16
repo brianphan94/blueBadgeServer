2168c1ca01fd66c8fd07ecaaa2a65079b5be99b6
aymiller200 <aymiller200@gmail.com>
2021-05-16 13:46:20 -0400

    went ahead and finished up the user login. Was going to see if I could get a mock up of the landing user register/login page going today

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -17,9 +17,30 @@ router.post('/register', (req, res) => {
         let token = jwt.sign({id: user.id}, "I_AM_SECRET",{expiresIn: '1d'})
         res.status(200).send({user:user, token: token})
     })
-
-
     .catch((error => res.status(500).send({error: error})))
+})
+
+//USER LOGIN
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.user.email
+        }
+    })
+    .then(user => {
+        if(user){
+            bcrypt.compare(req.body.user.password, user.password, function(err, matches){
+                matches ? generateToken(user) : res.send('Email or password is incorrect.')
+            })
+
+            function generateToken(user) {
+                let token = jwt.sign({id: user.id}, "I_AM_SECRET", {expiresIn: '1d'})
+                res.send({user, token})
+            }
+        }else{
+            res.send('No user detected')
+        }
+    }).catch(err => res.status(500).json({error: err}))
 })
 
 module.exports = router
