e2d892f52232a3e769fef0dc8e5a4e2c3b171891
brianphan94 <brianphan317@gmail.com>
2021-05-18 18:34:50 -0400

    fixed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,8 @@
+  
 const router = require('express').Router();
 const User = require('../models/user');
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
-const validate = require('../middleware/validateSession');
 
 router.get('/testing', (req, res) => {
     res.send('testing testing')
@@ -11,7 +11,6 @@ router.get('/testing', (req, res) => {
 //USER CREATE
 router.post('/register', (req, res) => {
     User.create({
-        username: req.body.user.username,
         email: req.body.user.email,
         password: bcrypt.hashSync(req.body.user.password, 10)
     })
@@ -22,20 +21,11 @@ router.post('/register', (req, res) => {
     .catch((error => res.status(500).send({error: error})))
 })
 
-//!LIST USERS
-router.get('/users', (req, res) => {
-    User.findAll()
-    .then(user => res.status(200).json({user}))
-    .catch(err => res.status(500).json({message: 'Could not find any users.', error: err}))
-})
-//! NEED TO ADD TOKEN IN THE VIEW USER ENDPOINT
-
 //USER LOGIN
 router.post('/login', (req, res) => {
     User.findOne({
         where: {
-            username: req.body.user.username,
-            password: req.body.user.password
+            email: req.body.user.email
         }
     })
     .then(user => {
@@ -52,13 +42,6 @@ router.post('/login', (req, res) => {
             res.send('No user detected')
         }
     }).catch(err => res.status(500).json({error: err}))
-})
-
-//!UPDATE USER
-router.put('/update/:id', validate, (req, res) => {
-    User.update(req.body, {where: {id: req.params.id}})
-    .then(updated => res.status(200).json({message: `Successfully updated user ${req.params.id}`, updated}))
-    .catch(err => res.status(500).json({message: 'Update unsuccessful, ', error: err}))
 })
 
 module.exports = router
--- server/models/user.js
+++ server/models/user.js
@@ -1,19 +1,16 @@
+  
 const sequelize = require('sequelize');
 const db = require('../../db')
 
 
 const User = db.define('user', {
-    username: {
-    type: sequelize.STRING,
-    allowNull: false,
-    unique: true
-    },
-    
     email: {
         type: sequelize.STRING,
         allowNull: false, 
         unique: true
     },
+
+    //maybe username also
 
     password: {
         type: sequelize.STRING, 
